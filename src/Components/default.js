47aefd99432b76caaefde769c1aa7f3ac825d92e
KarenQuezadaMejia <quezadakaren16@gmail.con>
2019-07-25 22:47:29 -0400

    se modifica template default

--- src/Components/default.js
+++ src/Components/default.js
@@ -5,30 +5,39 @@ import '../css/default.css';
 
 
 class Default extends Component {
- /*  getUser() {
-    db.collection("Users").get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            console.log(doc.id, " => ", doc.data());
-        });
-    });
-  }
-  componentDidMount() {
+  /*  getUser() {
+     db.collection("Users").get().then(function(querySnapshot) {
+         querySnapshot.forEach(function(doc) {
+             console.log(doc.id, " => ", doc.data());
+         });
+     });
+   }
+   componentDidMount() {
+ 
+     this.getUser()
+   } */
 
-    this.getUser()
-  } */
-  
-    
-    render() {
-  return (
+  render() {
+    return (
       <React.Fragment>
+        <div className="contentuser">
           <img src={group} class="mx-auto img-fluid rounded-circle" alt="Generic placeholder thumbnail"></img>
-                    <h1>Bienvenido</h1>
-                    <span class="text-muted">Device agnostic</span>
-       
-       
-    </React.Fragment>
-  );
-}
+          <h1>Bienvenido</h1>
+          <span class="text-muted">Device agnostic</span>
+          <div className="card-body">
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item" id="optionitem"><i class="fas fa-users"></i>Nuevos Postulantes</li>
+    <li class="list-group-item" id="optionitem"><i class="fas fa-check-square"></i>Publicaciones Activas</li>
+    <li class="list-group-item" id="optionitem"><i class="fas fa-hands-helping"></i> Tus Contrataciones</li>
+  </ul>
+  <p id="footerdefault">¿Tienes un nuevo puesto disponible?</p>
+  <button type="button" className="btn btn-light m-2" id="publish">HAZ UNA PUBLICACIÓN</button>
+          </div>
+        </div>
+
+      </React.Fragment>
+    );
+  }
 }
 
 export default Default
